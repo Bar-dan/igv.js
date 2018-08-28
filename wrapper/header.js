e538a98c5e8a56e48c818ef56dab8d71c6dacd50
dbarreca <daniele.barreca@gmail.com>
2018-08-28 13:49:58 +0200

    Removed dependency from jquery

--- wrapper/header.js
+++ wrapper/header.js
@@ -4,17 +4,18 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define([], factory);
+        define(['jquery'], factory);
     } else if (typeof module === 'object' && module.exports) {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
-        module.exports = factory();
+        module.exports = factory(require('jquery'));
     } else {
         // Browser globals (root is window)
-        root.igv = factory();
+        root.igv = factory(root.jQuery);
     }
- }(this, function () {
+ }(this, function ($) {
+    var jQuery = $;
 //
 //     // Just return a value to define the module export.
 //     // This example returns an object, but the module
@@ -22,5 +23,4 @@
 //     return igv;
 //
 // }));
-
 
